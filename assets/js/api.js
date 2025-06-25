49e79ad8ef18620406d16fe333a930f12927ded5
iaenblanco <nicolas@iaenblanco.com>
2025-06-24 22:47:09 -0400

    Fix: pagina de productos.

--- assets/js/api.js
+++ assets/js/api.js
@@ -119,7 +119,7 @@ function crearTarjetaPropiedad(propiedad) {
           <span>🚿 ${propiedad.Banos}</span>
           <span>🏡 ${propiedad.Superficie} m²</span>
         </div>
-        <a href="/propiedades/${propiedad.Slug}.html" class="btn">Ver Propiedad</a>
+        <a href="/propiedad-dinamica.html?slug=${propiedad.Slug}" class="btn">Ver Propiedad</a>
       </div>
     </article>
   `;
