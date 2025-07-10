42443bd2f2515ed03a05b53f55c2097b5302cf22
iaenblanco <nicolas@iaenblanco.com>
2025-07-10 00:18:46 -0400

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ¡AÑADE ESTA LÍNEA!
+  site: 'https://propinvest.cl', 
+  
+  integrations: [sitemap()]
+});
